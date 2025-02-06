ce056f3f6743b67a7e8ca7bb1645239db0ecc15c
Grandis Mahendra W W <grandisGT@gmail.com>
2025-02-06 22:23:01 +0700

    Create Simple Rest API with Fiber use Middleware logger & cors

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+)
+
+func main() {
+	app := fiber.New()
+
+	// Middleware
+	app.Use(logger.New())
+	app.Use(cors.New())
+
+	// Routes
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.JSON(fiber.Map{"message": "Welcome to Fiber API"})
+	})
+
+	app.Listen(":8000")
+}
